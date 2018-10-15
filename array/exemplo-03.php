96e391cfbbb81c877467fdfebe529f5673e3c3ea
Mayco Vilefort <vilefort.vip@gmail.com>
2018-10-15 13:07:36 -0300

    Adicionei o sexo e nascimento a pessoa do arquivo exemplo-03.php

--- array/exemplo-03.php
+++ array/exemplo-03.php
@@ -5,7 +5,9 @@ $pessoa = array();
 array_push($pessoa, array(
 
 	'nome'=>'Mayco Vilefort',
-	'idade'=>30
+	'idade'=>30,
+	'sexo'=>'M',
+	'nascimento'=>'20/04/1988'
 ));
 
 array_push($pessoa, array(
